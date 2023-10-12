95896dde4a3482893f947f89f42d75383753d843
yxyHeart <56054482+yxyHeart@users.noreply.github.com>
2023-10-12 17:49:40 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,16 +1,7 @@
 
-const getPromise = ()=>{
-    return new Promise((res,rej)=>{
-        setTimeout(()=>rej('123'),2000)
-    })
-}
 
 const f = async()=>{
-    await getPromise().catch(err=>{
-        console.log(err)
-        // return 
-    })
-    console.log(1)
+    setInterval(()=>{console.log(new Date().getUTCDate())},2000)
 }
 
 f()
